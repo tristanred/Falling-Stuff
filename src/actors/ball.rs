56c9ba764129668600f11a2547b56923ee028eea
Tristan Dubé-Lepage <tristan.dube@gmail.com>
2021-10-16 16:42:55 -0400

    Ball wip

--- src/actors/ball.rs
+++ src/actors/ball.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
-pub fn setup_ball(mut cmd: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+pub fn setup_ball(
+    mut cmd: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>) {
+    let texture = asset_server.load("assets/actors/ball/ball.png");
+    cmd.spawn_bundle(OrthographicCameraBundle::new_2d());
+    cmd.spawn_bundle(SpriteBundle {
+        material: materials.add(texture.into()),
+        ..Default::default()
+    });
 
 }
